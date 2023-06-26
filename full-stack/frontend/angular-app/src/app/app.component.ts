1acaed9916fc711da44dc86ea06e29c65aa4c603
NikyDS <dossantosnikoly01@gmail.com>
2023-06-26 15:19:34 -0400

    before break

--- full-stack/frontend/angular-app/src/app/app.component.ts
+++ full-stack/frontend/angular-app/src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BankAccountComponent } from './components/bank-account.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavigationComponent } from "./components/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BankAccountComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, BankAccountComponent, HeadingComponent,
+    LandingPageComponent, NavigationComponent]
 })
 export class AppComponent {
 
