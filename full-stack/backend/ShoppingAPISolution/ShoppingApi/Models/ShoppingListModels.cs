a3d97c203ebb1b1874f0ad878b359be8832ffd69
NikyDS <dossantosnikoly01@gmail.com>
2023-06-28 16:38:49 -0400

    done with api almost

--- full-stack/backend/ShoppingAPISolution/ShoppingApi/Models/ShoppingListModels.cs
+++ full-stack/backend/ShoppingAPISolution/ShoppingApi/Models/ShoppingListModels.cs
@@ -1,4 +1,6 @@
-﻿namespace ShoppingApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingApi.Models;
 
 public record ShoppingListItemModel
 {
@@ -11,4 +13,10 @@ public record CollectionResponse<T>
 {
     public List<T> Data { get; set; } = new();
 
+}
+
+public record ShoppingListItemCreateModel
+{
+    [Required, MinLength(3), MaxLength(50)]
+    public string Description { get; init; } = string.Empty;
 }
