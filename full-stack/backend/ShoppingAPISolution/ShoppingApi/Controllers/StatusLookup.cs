80ecfa62ec0721462910bc9a621ab05ac451f87c
NikyDS <dossantosnikoly01@gmail.com>
2023-06-28 13:58:13 -0400

    before lunch

---
+++ full-stack/backend/ShoppingAPISolution/ShoppingApi/Controllers/StatusLookup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApi.Data;
+
+namespace ShoppingApi.Controllers;
+
+public class StatusLookup : ILookupTheStatus
+{
+    private readonly ShoppingDataContext _context;
+    public async Task<GetStatusResponse> GetCurrentStatusAsync()
+    {
+        //var savedStatus = await _context.StatusMessages.OrderBy(m => m.LastChecked).FirstOrDefaultAsync();
+
+        //if(savedStatus is null)
+        //{
+        //    var entityToSave = new StatusEntity
+        //    {
+        //        LastChecked = DateTimeOffset.Now,
+        //        Message = "Looks Good"
+        //    };
+        //    _context.StatusMessages.Add(entityToSave);
+        //    await _context.SaveChangesAsync();
+
+        //}
+        //else
+        //{
+
+        //}
+
+        var response = new GetStatusResponse
+        {
+            Message = "Looks Good Here",
+            LastChecked = DateTimeOffset.Now
+        };
+        return response;
+    }
+}
---
+++ full-stack/backend/ShoppingAPISolution/ShoppingApi/Models/StatusModels.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingApi.Models;
+
+
+public record GetStatusResponse
+{
+    public string Message { get; init; } = string.Empty;
+    public DateTimeOffset LastChecked { get; init; }
+}
